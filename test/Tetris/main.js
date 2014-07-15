4b70ad3bd17fc4abbd6f4f97294ed47c2cc80e01
nihca <nihca@live.cn>
2014-07-15 18:48:00 +0800

    tab to space

--- test/Tetris/main.js
+++ test/Tetris/main.js
@@ -1,7 +1,7 @@
 (function(window, $, Class, util, Canvas, undefined){
-	var canvas = new Canvas($('#canvas'), '2d');
+    var canvas = new Canvas($('#canvas'), '2d');
 
-	var doc = canvas.document,
-		body = doc.body;
+    var doc = canvas.document,
+        body = doc.body;
 
 })(this, $, Class, util, Canvas);
